38d9f5da15c73859a7bf591ee167ad0c4e028fb1
Alexander Wang <aw576@cornell.edu>
2021-03-21 01:37:36 -0400

    implemented programatically loading FEN strings

---
+++ alphaviz.py
@@ -0,0 +1,333 @@
+from typing import cast
+import constants
+import validateFEN
+import tkinter as tk
+from PIL import Image, ImageTk
+import re
+
+DEBUG = True
+CORN = False
+
+class ChessGui(tk.Frame):
+    # GUI display variables
+    selected = None # selected square
+    selected_piece = None
+    highlighted = None
+
+    # Initializes the AlphaViz GUI
+    # Sets many instance variables, loads images used for the GUI, creates
+    # most GUI components, and prepares the GUI for human interaction.
+    def __init__(self, parent):
+        if DEBUG:
+            print("DEBUG MODE: ON")
+            print("ChessGui.__init__() executing...")
+        # Set parent GUI element (most likely root)
+        self.parent = parent
+
+        # Initialize tk.Frame of root
+        tk.Frame.__init__(self, parent)
+
+        # Initialize Game State Variables
+        self.reset_board_state()
+
+        # Load the images used
+        self.load_images()
+
+        # Get the width and height of the chessboard to define the initial 
+        # size of the canvas
+        canvas_width = self.IMG_CHESSBOARD.width()
+        canvas_height = self.IMG_CHESSBOARD.height()
+
+        # Create the canvas component
+        self.canvas = tk.Canvas(self, width=canvas_width, height=canvas_height, background="white")
+        self.canvas.pack(side="top", fill="both", anchor="c", expand=True)
+
+        # Draw the chessboard on the canvas
+        self.draw_board()
+
+        # Directs configuration event of the canvas to call self.refresh()
+        self.canvas.bind("<Configure>", self.refresh)
+        # Directs left mouse click event to call self.click()
+        self.canvas.bind("<Button-1>", self.click)
+
+        # Create GUI frame at the bottom of the root frame, 
+        # below the chessboard canvas
+        self.statusbar = tk.Frame(self, height=64)
+
+        # Create new game button and link it to the function self.reset()
+        self.button_new = tk.Button(self.statusbar, text="NEW", fg="black", command=self.reset)
+        self.button_new.pack(side=tk.LEFT, in_=self.statusbar)
+
+        # Create save game button and link it to the function [NONE] currently...
+        self.button_save = tk.Button(self.statusbar, text="CLEAR", fg="black", command=self.clear_board)
+        self.button_save.pack(side=tk.LEFT, in_=self.statusbar)
+
+        # [TEMPORARY DEMO FEATURE]
+        self.button_immortal = tk.Button(self.statusbar, text='IMMORTAL', fg="black", command=self.immortal)
+        self.button_immortal.pack(side=tk.LEFT, in_=self.statusbar)
+
+        # Display black/white's turn
+        self.label_status = tk.Label(self.statusbar, 
+                text="   White to move  " if self.whitetomove else "   Black to move  ", 
+                fg="black")
+        self.label_status.pack(side=tk.LEFT, expand=0, in_=self.statusbar)
+
+        # Create the quit button and link it to self.parent.destroy() function
+        self.button_quit = tk.Button(self.statusbar, text="Quit", fg="black", command=self.exit)
+        self.button_quit.pack(side=tk.RIGHT, in_=self.statusbar)
+        self.statusbar.pack(expand=False, fill="x", side='bottom')
+
+    def load_images(self):
+        self.IMG_CHESSBOARD = ImageTk.PhotoImage(file=constants.PATH_CHESSBOARD)
+        self.IMG_LTKING = ImageTk.PhotoImage(file=constants.PATH_LTKING)
+        self.IMG_DKKING = ImageTk.PhotoImage(file=constants.PATH_DKKING)
+        self.IMG_LTQUEEN = ImageTk.PhotoImage(file=constants.PATH_LTQUEEN)
+        self.IMG_DKQUEEN = ImageTk.PhotoImage(file=constants.PATH_DKQUEEN)
+        self.IMG_LTROOK = ImageTk.PhotoImage(file=constants.PATH_LTROOK)
+        self.IMG_DKROOK = ImageTk.PhotoImage(file=constants.PATH_DKROOK)
+        self.IMG_LTBISHOP = ImageTk.PhotoImage(file=constants.PATH_LTBISHOP)
+        self.IMG_DKBISHOP = ImageTk.PhotoImage(file=constants.PATH_DKBISHOP)
+        self.IMG_LTKNIGHT = ImageTk.PhotoImage(file= 
+                constants.PATH_LTUNICORN if CORN else constants.PATH_LTKNIGHT)
+        self.IMG_DKKNIGHT = ImageTk.PhotoImage(file=
+                constants.PATH_DKUNICORN if CORN else constants.PATH_DKKNIGHT)
+        self.IMG_LTPAWN = ImageTk.PhotoImage(file=constants.PATH_LTPAWN)
+        self.IMG_DKPAWN = ImageTk.PhotoImage(file=constants.PATH_DKPAWN)
+
+    def load_fen(self, fen_string):
+        if DEBUG:
+            print(f'ChessGui.load_fen(\'{fen_string}\') executing...')
+        
+        self.reset_board_state()
+
+        if validateFEN.fenPass(fen_string):
+            if DEBUG:
+                print(f'\'{fen_string}\' is a valid FEN string!')
+
+            # Tokenize 
+            tokens = fen_string.split(' ')
+            board_str = tokens[0]
+            bwturn_str = tokens[1]
+            castles_str = tokens[2]
+            enpassant_str = tokens[3]
+            halfmoves_str = tokens[4]
+            moves_str = tokens[5]
+
+            # set self.whitetomove
+            if bwturn_str == 'w':
+                self.whitetomove = True
+            else:
+                self.whitetomove = False
+
+            # set en passant move
+            self.possible_enpassant = enpassant_str
+            # set the half moves and moves
+            self.half_moves = int(halfmoves_str)
+            self.moves = int(moves_str)
+
+            # parse castling
+            if 'K' in castles_str:
+                self.wk_castle = True
+            if 'Q' in castles_str:
+                self.wq_castle = True
+            if 'k' in castles_str:
+                self.bk_castle = True
+            if 'q' in castles_str:
+                self.bq_castle = True
+
+            # parse board
+            # DO NOT TWEAK THESE NUMBERS THEY WERE CAREFULLY MANUFACTURED
+            i = 56
+            for character in board_str:
+                if character == '/':
+                    i -= 16
+                elif character.isdigit():
+                    i += int(character)
+                elif character == 'P':
+                    self.board[i] = constants.LTPAWN
+                    i += 1
+                elif character == 'p':
+                    self.board[i] = constants.DKPAWN
+                    i += 1
+                elif character == 'R':
+                    self.board[i] = constants.LTROOK
+                    i += 1
+                elif character == 'r':
+                    self.board[i] = constants.DKROOK
+                    i += 1
+                elif character == 'K':
+                    self.board[i] = constants.LTKING
+                    i += 1
+                elif character == 'k':
+                    self.board[i] = constants.DKKING
+                    i += 1
+                elif character == 'B':
+                    self.board[i] = constants.LTBISHOP
+                    i += 1
+                elif character == 'b':
+                    self.board[i] = constants.DKBISHOP
+                    i += 1
+                elif character == 'Q':
+                    self.board[i] = constants.LTQUEEN
+                    i += 1
+                elif character == 'q':
+                    self.board[i] = constants.DKQUEEN
+                    i += 1
+                elif character == 'N':
+                    self.board[i] = constants.LTKNIGHT
+                    i += 1
+                elif character == 'n':
+                    self.board[i] = constants.DKKNIGHT
+                    i += 1
+        else:
+            if DEBUG:
+                print(f'{fen_string} is NOT a valid FEN string!')
+                print('[ERROR] load_fen() Failed.')
+        
+
+    def board_to_fen(self):
+        if DEBUG:
+            print('ChessGui.board_to_fen() executing...')
+
+    def click(self, event):
+        if DEBUG:
+            print('ChessGui.click() executing...')
+
+    def move(self, p1, p2):
+        if DEBUG:
+            print('ChessGui.move() executing...')
+
+    def highlight(self, pos):
+        if DEBUG:
+            print('ChessGui.highlight() executing...')
+
+    def addpiece(self, name, image, row=0, column=0):
+        if DEBUG:
+            print('ChessGui.addpiece() executing...')
+
+    def placepiece(self, name, row, column):
+        if DEBUG:
+            print('ChessGui.placepiece() executing...')
+
+    def refresh(self, event={}):
+        # NEED TO IMPLEMENT!!
+        if DEBUG:
+            print('ChessGui.refresh() executing...')
+
+    def draw_pieces(self):
+        if DEBUG:
+            print('ChessGui.draw_pieces() executing...')
+        # Starting pixel counts for A1-square
+        x_pos = constants.BOARD_OFFSET # = 30
+        y_pos = constants.BOARD_SIZE - constants.BOARD_OFFSET - constants.SQUARE_SIZE 
+        # = 590
+
+        for rank in range(0,8):
+            for file in range(0, 8):
+                ind = rank*8 + file
+
+                # The ordering of these if statements were created in the wee hours
+                # of the night by Zander. The logic is strange but they matter 
+                # for efficiency. If you are unconvinced, that is okay.
+                # The gist of it is that we check the most likely pieces first
+
+                if self.board[ind] == constants.EMPTY:
+                    # Don't draw anything if the square is empty
+                    pass
+                elif self.board[ind] == constants.LTPAWN:
+                    self.canvas.create_image(x_pos,y_pos,anchor='nw',image=self.IMG_LTPAWN)
+                elif self.board[ind] == constants.DKPAWN:
+                    self.canvas.create_image(x_pos,y_pos,anchor='nw',image=self.IMG_DKPAWN)
+                elif self.board[ind] == constants.LTROOK:
+                    self.canvas.create_image(x_pos,y_pos,anchor='nw',image=self.IMG_LTROOK)
+                elif self.board[ind] == constants.DKROOK:
+                    self.canvas.create_image(x_pos,y_pos,anchor='nw',image=self.IMG_DKROOK)
+                elif self.board[ind] == constants.LTKING:
+                    self.canvas.create_image(x_pos,y_pos,anchor='nw',image=self.IMG_LTKING)
+                elif self.board[ind] == constants.DKKING:
+                    self.canvas.create_image(x_pos,y_pos,anchor='nw',image=self.IMG_DKKING)
+                elif self.board[ind] == constants.LTBISHOP:
+                    self.canvas.create_image(x_pos,y_pos,anchor='nw',image=self.IMG_LTBISHOP)
+                elif self.board[ind] == constants.DKBISHOP:
+                    self.canvas.create_image(x_pos,y_pos,anchor='nw',image=self.IMG_DKBISHOP)
+                elif self.board[ind] == constants.LTQUEEN:
+                    self.canvas.create_image(x_pos,y_pos,anchor='nw',image=self.IMG_LTQUEEN)
+                elif self.board[ind] == constants.DKQUEEN:
+                    self.canvas.create_image(x_pos,y_pos,anchor='nw',image=self.IMG_DKQUEEN)
+                elif self.board[ind] == constants.LTKNIGHT:
+                    self.canvas.create_image(x_pos,y_pos,anchor='nw',image=self.IMG_LTKNIGHT)
+                elif self.board[ind] == constants.DKKNIGHT:
+                    self.canvas.create_image(x_pos,y_pos,anchor='nw',image=self.IMG_DKKNIGHT)
+                else:
+                    print("[ERROR] ChessGui.draw_pieces(): board[ind] value matches no piece!")
+
+                x_pos += constants.SQUARE_SIZE
+            # End inner for loop    
+            x_pos = constants.BOARD_OFFSET
+            y_pos -= 80
+        # End outer for loop
+    # End draw_pieces()
+
+    def draw_board(self):
+        if DEBUG:
+            print('ChessGui.draw_board() executing...')
+        self.canvas.create_image(0,0,anchor='nw',image=self.IMG_CHESSBOARD)
+
+    def reset_board_state(self):
+        self.board = [0]*64 # initialize empty board. index 0 is A1 and 63 is H8
+        self.whitetomove = True
+        self.wk_castle = True           # Not yet implemented in load_fen()
+        self.wq_castle = True           # Not yet implemented in load_fen()
+        self.bk_castle = True           # Not yet implemented in load_fen()
+        self.bq_castle = True           # Not yet implemented in load_fen()
+        self.possible_enpassant = '-'   # Not yet implemented in load_fen()
+        self.half_moves = 0             # Not yet implemented in load_fen()
+        self.full_moves = 1             # Not yet implemented in load_fen()
+
+    def immortal(self):
+        if DEBUG:
+            print('ChessGui.immortal() executing...')
+        self.canvas.delete('all')
+        self.draw_board()
+        self.load_fen(constants.FEN_IMMORTAL_GAME)
+        self.draw_pieces()
+
+    def clear_board(self):
+        if DEBUG:
+            print('ChessGui.clear_board() executing...')
+        
+        self.canvas.delete('all')
+        self.draw_board()
+        self.load_fen(constants.FEN_EMPTY)
+        self.draw_pieces()
+
+    def reset(self):
+        if DEBUG:
+            print('ChessGui.reset() executing...')
+
+        # Reset board state
+        self.canvas.delete('all')
+        self.draw_board()
+        self.load_fen(constants.FEN_STARTING_POSITION)
+        self.draw_pieces()
+        
+    def exit(self):
+        if DEBUG:
+            print("ChessGui.exit() executing...")
+        self.parent.destroy()
+
+def display():
+    root = tk.Tk()
+    root.title('AlphaViz')
+    root.resizable(width=False, height=False)
+
+    gui = ChessGui(root)
+    gui.pack(side="top", fill="both", expand="true", padx=4, pady=4)
+    root.mainloop()
+    if DEBUG:
+        print('display() completed gracefully');
+
+if __name__ == "__main__":
+    display()
+    if DEBUG:
+        print('chess_gui main completed gracefully')
---
+++ constants.py
@@ -0,0 +1,50 @@
+# A declaration of all the constants used in this project
+
+FEN_STARTING_POSITION = 'rnbqkbnr/pppppppp/8/8/8/8/PPPPPPPP/RNBQKBNR w KQkq - 0 1'
+FEN_EMPTY = '4k3/8/8/8/8/8/8/4K3 b - - 0 1'
+FEN_1E4 = 'rnbqkbnr/pppppppp/8/8/4P3/8/PPPP1PPP/RNBQKBNR b KQkq e3 0 1'
+FEN_IMMORTAL_GAME = 'r1bk2nr/p2p1pNp/n2B1Q2/1p1NP2P/6P1/3P4/P1P1K3/q5b1 w - - 0 1'
+
+
+BOARD_SIZE = 700 # pixel width and height of chessboard.png
+BOARD_OFFSET = 30 # pixels of margin between board and edge in chessboard.png
+SQUARE_SIZE = 80 # Each square of our chess board is 80x80 pixels
+
+
+# Board size? idk where we might need this
+NUMROWS = 8
+NUMCOLS = 8
+
+# Numerical codes for the type of piece
+EMPTY = 0
+
+LTKING = 1
+LTQUEEN = 2
+LTROOK = 3
+LTBISHOP = 4
+LTKNIGHT = 5
+LTPAWN = 6
+
+DKKING = 7
+DKQUEEN = 8
+DKROOK = 9
+DKBISHOP = 10
+DKKNIGHT = 11
+DKPAWN = 12
+
+# Paths for all the images used in this project
+PATH_CHESSBOARD = r'imgs/chessboard_nbkgd.png'
+PATH_LTKING = r'imgs/king_light.png'
+PATH_DKKING = r'imgs/king_dark.png'
+PATH_LTQUEEN = r'imgs/queen_light.png'
+PATH_DKQUEEN = r'imgs/queen_dark.png'
+PATH_LTROOK = r'imgs/rook_light.png'
+PATH_DKROOK = r'imgs/rook_dark.png'
+PATH_LTBISHOP = r'imgs/bishop_light.png'
+PATH_DKBISHOP = r'imgs/bishop_dark.png'
+PATH_LTKNIGHT = r'imgs/knight_light.png'
+PATH_DKKNIGHT = r'imgs/knight_dark.png'
+PATH_LTPAWN = r'imgs/pawn_light.png'
+PATH_DKPAWN = r'imgs/pawn_dark.png'
+PATH_LTUNICORN = r'imgs/unicorn_light.png'
+PATH_DKUNICORN = r'imgs/unicorn_dark.png'
